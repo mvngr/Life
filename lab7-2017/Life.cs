b613401354d14c1d736572dbc4e1ade8de33b61d
Mike <mihailqqqq@gmail.com>
2017-11-27 20:49:14 +0300

    + lab

---
+++ lab7-2017/Life.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab7_2017
+{
+    public class Life
+    {
+        private int OX;
+        private int OY;
+        private byte[,] a;
+
+        private const ConsoleColor ALIVE_COLOR = ConsoleColor.White;
+        private const ConsoleColor DIED_COLOR = ConsoleColor.Black;
+        
+        public Life(int width, int height)
+        {
+            OX = width;
+            OY = height;
+            a = new byte[OY, OX];
+            Random r = new Random();
+            for (int i = 0; i < OY; i++)
+            for (int j = 0; j < OX; j++)
+                a[i, j] = (byte)(r.Next() % 5 == 0 ? 1 : 0);
+        }
+
+        public Life(int width, int height, byte chance)
+        {
+            OX = width;
+            OY = height;
+            a = new byte[OY, OX];
+            Random r = new Random();
+            for (int i = 0; i < OY; i++)
+            for (int j = 0; j < OX; j++)
+                a[i, j] = (byte)(r.Next() % chance == 0 ? 1 : 0);
+        }
+
+        public Life(byte[,] array)
+        {
+            OY = array.GetLength(0);
+            OX = array.GetLength(1);
+            a = array;
+        }
+
+        public void DebugIt()
+        {
+            Console.WriteLine("OX (width): " + OX);
+            Console.WriteLine("OY (height): " + OY);
+            for (int i = 0; i < OY; i++)
+            {
+                for (int j = 0; j < OX; j++)
+                    Console.Write(a[i,j] + " ");
+                Console.WriteLine();
+            }
+        }
+
+        public int correctData(int num, int size)
+        {
+            if (num >= size)
+                return 0;
+            if (num < 0)
+                return size - 1;
+            return num;
+        }
+
+        private byte getA(int y, int x)
+        {
+            int my = correctData(y, OY);
+            int mx = correctData(x, OX);
+            byte ans = a[my, mx];
+            return ans;
+        }
+
+        private byte sumAliveAround(int y, int x)
+        {
+            return (byte) (getA(y - 1, x - 1) + getA(y, x - 1) + getA(y + 1, x - 1) +
+                           getA(y - 1, x) + getA(y + 1, x) +
+                           getA(y - 1, x + 1) + getA(y, x + 1) + getA(y + 1, x + 1));
+        }
+
+        private bool isAlive(int y, int x)
+        {
+            if (getA(y, x) == 1)
+            {
+                int sum = sumAliveAround(y, x);
+                return sum == 2 || sum == 3 ? true : false; //for alive
+            }
+            else
+                return sumAliveAround(y, x) == 3 ? true : false; //for dead
+        }
+
+        public void oneStep()
+        {
+            byte[,] h = new byte[OY, OX];
+            for (int i = 0; i < OY; i++)
+            for (int j = 0; j < OX; j++)
+                h[i, j] = (byte) (isAlive(i, j) ? 1 : 0);
+            a = h;
+            return;
+        }
+
+        private void print()
+        {
+            for (int i = 0; i < OY; i++)
+            {
+                for (int j = 0; j < OX; j++)
+                    Console.Write(a[i, j] == 1 ? " o " : "   ");
+                Console.WriteLine();
+            }
+        }
+        
+        public void run()
+        {
+            while (true)
+            {
+                Console.Clear();
+                print();
+                Thread.Sleep(300);
+                oneStep();
+            }
+        
+        }
+    }
+}
---
+++ lab7-2017/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using lab7_2017.Properties;
+
+namespace lab7_2017
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Select starting field:\n" +
+                              "[0] Random (need input)\n" +
+                              "[1] Glider\n" +
+                              "[2] Middleweight Spaceship\n" +
+                              "[3] Glider Generator\n" +
+                              "[4] Prepulsar\n" +
+                              "[5] Unix\n" +
+                              "[6] Pi Portraitor");
+            Console.Write("Select: ");
+            int choice = Int32.Parse(Console.ReadLine());
+
+            Life l = null;
+            switch (choice)
+            {
+                case 1: l = new Life(Sample.Glider); break;
+                case 2: l = new Life(Sample.MiddleweightSpaceship); break;
+                case 3: l = new Life(Sample.GliderGenerator); break;
+                case 4: l = new Life(Sample.Prepulsar); break;
+                case 5: l = new Life(Sample.Unix); break;
+                case 6: l = new Life(Sample.PiPortraitor); break;
+                default: break;
+            }
+            if (l == null)
+            {
+                Console.Write("Input width and height (example: 4 4): ");
+                string[] mass = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                l = new Life(Int32.Parse(mass[0]), Int32.Parse(mass[1]));
+            }
+            Console.Clear();
+            l.run();
+        }
+    }
+}
